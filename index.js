69513004788361c0bd2fdab648cf272cd49e58ed
Catherine Njenga <freshiah.catherine@gmail.com>
2024-11-06 08:56:28 +0300

    read static files functionality

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(morgan('tiny'))
 morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
   { 
