5c2cb088d801e6dbd6dbdd7aa7d7f70cc29b1bce
Catherine Njenga <freshiah.catherine@gmail.com>
2024-11-06 07:49:10 +0300

    updating server cors status

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors())
 
 let persons = [
   { 
